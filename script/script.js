9110801b7dc80455e32d3df0e063c2cc980a915b
S. Manongga <smanongga@gmail.com>
2017-03-12 22:17:33 +1300

    Add algorithm to show correct message

--- script/script.js
+++ script/script.js
@@ -1,16 +1,30 @@
+/** Declare random number between 1 and 100 **/
 var max = 100;
 var min = 0;
 
 var randomNumber = Math.floor(Math.random() * (max - min)) + min;
 
+
 var numberTurn = 1;
 
 
 
-function guessMyNumber () {
-  var submmitedNumber = document.guessThatNumberForm.guessedNumber.value;
 
-  var showNumber = submmitedNumber + ', ';
+function guessMyNumber() {
 
+  var submittedNumber = document.guessThatNumberForm.guessedNumber.value;
+
+  var showNumber = submittedNumber + ' ' + 'Random Number: ' + randomNumber;
   document.getElementById("pastGuesses").innerHTML += showNumber;
+
+   if (submittedNumber == randomNumber) {
+     document.getElementById("congratulation").innerHTML = 'Congratulation! You guessed correctly.';
+   }
+   else if (submittedNumber < randomNumber ){
+      document.getElementById("hintMessage").innerHTML = 'You are too low!';
+   }
+   else if  (submittedNumber > randomNumber ){
+     document.getElementById("hintMessage").innerHTML = 'You are too high!';
+     }
+
 }
